069b828a6187be75139b461806170a9e515c66c2
Mohamad Javad Najafi <najafi.mohamad@outlook.com>
2016-04-12 20:48:48 +0430

    MY First Try

---
+++ GettingStarted/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShow_Click(object sender, RoutedEventArgs e)
+        {
+            string name = textBoxName.Text;
+            MessageBox.Show(name + " Jan khosh omadi");
+
+            string currentuser = textBoxName.Text;
+            textBoxcurrentuser.Text = currentuser;
+        }
+
+        private void buttonLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Log Out Sucsessful");
+            textBoxcurrentuser.Text = "Not Loged in";
+        }
+    }
+}
