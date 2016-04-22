fdbdf700a4667a5adfad0017c93ed0e3497499cb
Mohamad Javad Najafi <najafi.mohamad@outlook.com>
2016-04-22 19:33:44 +0430

    event test

---
+++ eventTestProject/eventTestProject/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eventTestProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "text.txt");
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+                
+                
+
+        {
+            if (!File.Exists(filePath))
+            {
+                System.IO.StreamWriter tw = File.CreateText(filePath);
+                tw.Close();
+                
+            }
+            else
+            {
+                textBoxReadText.Text = File.ReadAllText(filePath);
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            editText editTextwin = new editText();
+            editTextwin.Show();
+            editTextwin.textAdded += EditTextwin_textAdded;
+            
+
+        }
+
+        private void EditTextwin_textAdded(object sender, EventArgs e)
+        {
+            textBoxReadText.Text = File.ReadAllText(filePath);
+        }
+    }
+}
---
+++ eventTestProject/eventTestProject/editText.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace eventTestProject
+{
+    /// <summary>
+    /// Interaction logic for editText.xaml
+    /// </summary>
+    public partial class editText : Window
+    {
+        public editText()
+        {
+            InitializeComponent();
+        }
+        
+        public event EventHandler textAdded;
+
+        
+
+        protected virtual void OnTextAdded()
+        {
+            textAdded?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string textBoxContent = textBoxEditText.Text;
+            string filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "text.txt");
+            TextWriter tw = new StreamWriter(filePath,true);
+            tw.WriteLine(textBoxContent);
+            tw.Close();
+            OnTextAdded();
+            this.Close();
+        }
+    }
+}
